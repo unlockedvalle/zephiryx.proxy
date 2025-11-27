1c9a6501f9ea291d931fbe4fb438a8357d571f70
unlockedvalle <unlockedvalle@gmail.com>
2025-11-27 20:08:36 +0100

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 // uv.config.js → Versión FUNCIONAL 100% (Noviembre 2025)
 self.__uv$config = {
     prefix: '/service/',                    // ← Correcto y estándar actual
-    bare: '/bare/',                         // ← Cambia si tu bare está en otro dominio
+    bare: 'https://zephiryx-bare-production.up.railway.app/bare',                         // ← Cambia si tu bare está en otro dominio
     encodeUrl: Ultraviolet.codec.xor.encode, // ← ESTE ES EL ÚNICO QUE FUNCIONA BIEN HOY
     decodeUrl: Ultraviolet.codec.xor.decode, // ← Obligatorio para Discord, YouTube, etc.
     handler: '/uv/uv.handler.js',
